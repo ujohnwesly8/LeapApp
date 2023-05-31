55f44d6fffcc95c69606c67a99dae4b77e27763f
johnwesly2002 <ujohnwesly8@gmail.com>
2023-05-31 23:00:04 +0530

    HomeScreen completed

--- src/screens/Home/Homescreen.tsx
+++ src/screens/Home/Homescreen.tsx
@@ -1,4 +1,3 @@
-
 /* eslint-disable react/self-closing-comp */
 /* eslint-disable react-native/no-inline-styles */
 import {
@@ -10,6 +9,7 @@ import {
   TextInput,
   RefreshControl,
   FlatList,
+  useColorScheme,
 } from 'react-native';
 import React, {useContext, useEffect, useState} from 'react';
 import Carousal from './Carousal';
@@ -19,6 +19,10 @@ import {
 } from '../../redux/actions/actions';
 
 import style from './homeStyles';
+import Icon from 'react-native-vector-icons/AntDesign';
+import MaterialIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Lottie from 'lottie-react-native';
+import {useDispatch, useSelector} from 'react-redux';
 import useHome from './useHome';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import Colors from '../../constants/Colors';
@@ -43,6 +47,46 @@ const Homescreen = ({navigation}: Props) => {
     loading,
     // recommendations,
   } = useHome();
+  const allProducts = useSelector(state => state.UserProducts.data);
+  // const [showModal, setShowModal] = useState(false);
+  const [wishlistList, setWishlistList] = useState([]);
+
+  // console.log(allProducts);
+  // const handleHeartClick = item => {
+  //   dispatch(postProductToAPI(item));
+  //   console.log('success');
+  // };
+  // const openModal = () => {
+  //   setShowModal(true);
+  // };
+  // const closeModal = () => {
+  //   setShowModal(false);
+  // };
+
+  if (!UserProducts) {
+    return (
+      <View
+        style={{
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100%',
+        }}>
+        <Lottie
+          source={require('../../../assets/loading.json')}
+          autoPlay
+          style={{
+            height: 200,
+            width: 200,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        />
+        <Text>The Items are Loading...</Text>
+      </View>
+    );
+  }
+
+  console.log('indranil', allProducts);
 
   return (
     <SafeAreaView
@@ -52,22 +96,139 @@ const Homescreen = ({navigation}: Props) => {
         overflow: 'scroll',
       }}>
       {loading ? (
+        <SkeletonPlaceholder
+          highlightColor="#e0e0e0"
+          {/* <View
+              style={[
+                style.mainContainer,
+                colorScheme === 'dark' ? style.blacktheme : style.whiteTheme,
+              ]}> */}
+          <Text
+            style={{
+              marginLeft: 26,
+              marginTop: 10,
+              width: 70,
+              // fontWeight: '900',
+              fontFamily: 'Poppins-SemiBold',
+              fontSize: 15,
+            }}></Text>
+          <View
+            style={[
+              style.searchInputContainer,
+            ]}>
+            <TextInput
+              placeholder="Search"
+              placeholderTextColor={
+              }
+              style={{
+                // height: 10,
+                borderRadius: 40,
+                fontFamily: 'Poppins-Regular',
+                fontSize: 16,
+                width: '90%',
+                height: 45,
+                marginTop: 8,
+                paddingLeft: 10,
+                // color: 'black',
+              }}
+            />
+          </View>
+          <Text
+            style={{
+              marginLeft: 26,
+              marginTop: 30,
+              width: 300,
+              height: 25,
+              borderRadius: 50,
+              // fontWeight: '900',
+              fontFamily: 'Poppins-SemiBold',
+              fontSize: 15,
+            }}></Text>
+          <View style={style.categoriesContainer}>
+            <Text
+              style={[
+                style.CategoriesText,
+              ]}></Text>
+            <TouchableOpacity>
+              <Text style={style.Seetext}></Text>
+            </TouchableOpacity>
+          </View>
+          <View style={{flexDirection: 'row'}}>
+            <View>
+              <TextInput style={style.img}></TextInput>
+            </View>
+            <View>
+              <TextInput style={style.img}></TextInput>
+            </View>
+            <View>
+              <TextInput style={style.img}></TextInput>
+            </View>
+            <View>
+              <TextInput style={style.img}></TextInput>
+            </View>
+            <View>
+              <TextInput style={style.img}></TextInput>
+            </View>
+          </View>
+          <View style={[style.container, {marginTop: 70}]}>
+            <TouchableOpacity>
+              <View style={style.imageContainer}>
+                <TextInput style={style.image}></TextInput>
+                <TouchableOpacity style={style.wishlistButton}>
+                  <MaterialIcons size={20} color={'red'} name="cards-heart" />
+                </TouchableOpacity>
+              </View>
+            </TouchableOpacity>
+            {/* <View style={style.cardTextContainer}> */}
+            <Text style={style.name}></Text>
+            {/* </View> */}
+            {/* <View style={style.textContainer}> */}
+            <Text style={style.price}></Text>
+            {/* </View> */}
+            {/* </View> */}
+          </View>
+          {/* </View> */}
+        </SkeletonPlaceholder>
       ) : (
         <View
           style={[
             style.mainContainer,
           ]}>
           {/* <Header title={'Leap'} /> */}
+          <View style={{flexDirection: 'row'}}>
+            <Text
+              style={{
+                marginLeft: 26,
+                marginTop: 10,
+                // fontWeight: '900',
+                fontFamily: 'Poppins-SemiBold',
+                fontSize: 15,
+              }}>
+              Welcome {name}
+            </Text>
+            <Lottie
+              source={require('../../../assets/celebration.json')}
+              autoPlay
+              style={{height: 45, width: 50}}
+            />
             <View
               style={{
                 flexDirection: 'row',
                 marginTop: 10,
                 marginLeft: 20,
               }}></View>
+          </View>
           <View
             style={[
               style.searchInputContainer,
             ]}>
+            <Icon
+              name="search1"
+              size={20}
+              style={{
+                marginLeft: 20,
+              }}
+            />
             <TextInput
               placeholder="Search"
               placeholderTextColor={
