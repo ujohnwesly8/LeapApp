f1b69dd5b2270208e186211646d03734f2cd2cc6
Yuvakishore123 <kolli.yuva@nineleaps.com>
2023-06-01 00:55:39 +0530

    cart functionalitues custom hook added

--- src/screens/Cart/useCart.tsx
+++ src/screens/Cart/useCart.tsx
@@ -18,18 +18,106 @@ import RazorpayCheckout from 'react-native-razorpay';
 import ApiService from '../../network/network';
 import {ColorSchemeContext} from '../../../ColorSchemeContext';
 function useCart() {
-
+  // const {product} = route.params;
+  const [rentalStartDate, setRentalStartDate] = useState(new Date());
+  const [rentalEndDate, setRentalEndDate] = useState(new Date());
   // const [quantity, setQuantity] = useState(1);
   const [isloading, setIsLoading] = useState(false);
+  const [isplusDisable, setisButtondisable] = useState(false); // Added loading state
   const navigation = useNavigation();
   // const colorScheme = useColorScheme();
+  const {colorScheme} = useContext(ColorSchemeContext);
   const isLoading = useSelector(state => state.CartProducts.isLoader);
-  const cartData = useSelector(state => state.CartProducts.data);
 
+  // const CartData = useSelector(state => state.CartProducts.data) || {
+  //   cartItems: [],
+  // };
+
+  // const itemQuantity = cartData;
+  // console.log('itemQuantity is :', itemQuantity?.cartItems);
+
+  // if (Array.isArray(itemQuantity?.cartItems)) {
+  //   const quantities = itemQuantity.cartItems.map(item => item.quantity);
+  //   console.log('Quantities:', quantities);
+  // }
+  // const [quantity, setQuantity] = useState(1);
+
+  const [quantity, setQuantity] = useState(1);
+  const [Productquantity, setProductQuantity] = useState<number[]>([]);
+
+  // useEffect(() => {
+  //   // Get the quantity from the cart data
+  //   const quantities =
+  //     cartData?.cartItems.map(item => parseInt(item.quantity, 10)) || [];
+  //   const joinedQuantity = quantities.join(', ');
+  //   console.log('Quantity:', quantities[0]);
+  //   setQuantity(joinedQuantity[0]);
+  // }, [cartData]);
+
+  // console.log('Type of quantity:', typeof quantity);
+
+  // Example usage
   console.log('Quantity:', quantity);
+
+  // const [refreshing, setRefreshing] = useState(false);
+
+  // const Qunatity = cartData;
+
+  // const onRefresh = async () => {
+  //   setRefreshing(true);
+  //   await dispatch(fetchCartProducts());
+  //   setRefreshing(false);
+  // };
+  // const handleUpdate = async () => {
+  //   try {
+  //     const token = await AsyncStorage.getItem('token');
+  //     const cartItems = cartData?.cartItems;
+  //     if (!cartItems || cartItems.length === 0) {
+  //       console.log('Cart is empty, cannot update');
+  //       return;
+  //     }
+  //     const items = {
+  //       cartItems: cartItems.map(item => ({
+  //         productId: item.product.id,
+  //         quantity: item.quantity, // Use the new quantity
+  //       })),
+  //     };
+  //     const Newdata = items.cartItems[0];
+  //     console.log('items data is', Newdata);
+  //     const response = await fetch(QuantityApi, {
+  //       method: 'PUT',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //         Authorization: `Bearer ${token}`,
+  //       },
+  //       body: JSON.stringify(Newdata),
+  //     });
+  //     const data = await response.json();
+  //     console.log('Update response:', data);
+  //   } catch (error) {
+  //     console.error('Update error:', error);
+  //   }
+  // };
   const handleUpdate = async (newQuantity, productId) => {
     try {
       const token = await AsyncStorage.getItem('token');
+      // const cartItems = cartData?.cartItems;
+      // if (!cartItems || cartItems.length === 0) {
+      //   console.log('Cart is empty, cannot update');
+      //   return;
+      // }
+      // const items = {
+      //   cartItems: cartItems.map(item => ({
+      //     productId: item.product.id,
+      //     quantity: item.quantity === newQuantity ? item.quantity : newQuantity,
+      //   })),
+      // };
+      const data = {
+        productId: productId,
+        quantity: newQuantity,
+      };
+
+      console.log('Important data is :', newQuantity, productId);
       const response = await fetch(QuantityApi, {
         method: 'PUT',
         headers: {
@@ -41,6 +129,7 @@ function useCart() {
       const Data = await response.json();
       console.log('Update response:', Data);
     } catch (error) {
+      // console.error('Update error:', error);
     }
   };
 
@@ -86,7 +175,6 @@ function useCart() {
       .then(data => {
         // console.log('Item removed from cart:', data);
         dispatch(removeFromCart(productId));
-        openModal();
       })
       .catch(error => {
         console.error(error);
@@ -128,6 +216,60 @@ function useCart() {
 
   const dispatch = useDispatch();
   const CartProducts = useSelector(state => state.CartProducts.data);
+  // console.log(JSON.stringify(CartProducts));
+  // console.log('cart succes data', CartProducts);
+  //Api call for products
+  // const selectUserProducts = useSelector(state => state.UserProducts.data);
+  // console.log('selectUserProducts', selectUserProducts);
+  // const Productid = cartData && cartData.cartItems;
+  // console.log('card data is for id:', Productid);
+
+  // const Productid = cartData && cartData.cartItems;
+  // console.log('card data is for id:', Productid);
+
+  // if (Productid && Array.isArray(Productid)) {
+  //   const productIds = Productid.map(item => item.product.id);
+  //   console.log('Product IDs:', productIds);
+
+  //   const fetchQuantityData = async () => {
+  //     try {
+  //       const result = await ApiService.get(`${ProductsById}/${productIds}`);
+  //       console.log('result of products is:', result.quantity);
+  //       setQuantity(result.quantity);
+  //     } catch (error) {
+  //       console.error('Error fetching quantity data:', error);
+  //     }
+  //   };
+
+  //   fetchQuantityData();
+  // }
+
+  // console.log(quantity);
+  const handleIncrement = useCallback(
+    item => {
+      const productId = item.product.id;
+      console.log('itemID', productId);
+      const productQuantity = item.product.availableQuantities;
+      console.log('Validation of product Quantity is ', productQuantity);
+      if (item.quantity === productQuantity) {
+        setisButtondisable(true);
+      } else {
+        const Quantity = item.quantity + 1;
+        console.log(Quantity);
+        handleUpdate(Quantity, productId);
+      }
+    },
+    [handleUpdate],
+  );
+
+  const handleDecrement = item => {
+    console.log(item.quantity);
+    const productId = item.product.id;
+    const newQuantity = item.quantity - 1;
+    console.log('itemID', productId);
+    handleUpdate(newQuantity, productId);
+    setisButtondisable(false);
+  };
 
   return {
     CartProducts,
@@ -135,6 +277,16 @@ function useCart() {
     handleRemove,
     handlePayment,
     handleUpdate,
+    rentalStartDate,
+    rentalEndDate,
+    setRentalStartDate,
+    setRentalEndDate,
+    colorScheme,
+    quantity,
+    setQuantity,
+    handleDecrement,
+    handleIncrement,
+    isplusDisable,
     isLoading,
     // fetchQuantityData,
   };
