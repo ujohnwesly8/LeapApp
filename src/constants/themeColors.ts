7911d46892f42d368cbfe39d2238e8c2ffbdac23
Pranay-Reddy007 <pentareddy.pranay@nineleaps.com>
2023-05-31 14:23:07 +0530

    themecolors added

---
+++ src/constants/themeColors.ts
@@ -0,0 +1,41 @@
+import {StyleSheet} from 'react-native';
+import Colors from './Colors';
+
+const Styles = StyleSheet.create({
+  blacktheme: {
+    backgroundColor: Colors.black,
+  },
+  whiteTheme: {
+    backgroundColor: Colors.main,
+  },
+  main: {
+    backgroundColor: Colors.white,
+  },
+  blackText: {
+    color: Colors.black,
+  },
+  whitetext: {
+    color: Colors.main,
+  },
+  InputText: {
+    color: Colors.Inputtext,
+  },
+  placeholder: {
+    color: Colors.gray,
+    // opacity: 0.5,
+  },
+  cardColor: {
+    backgroundColor: Colors.Textinput,
+  },
+  Cardcolor2: {
+    backgroundColor: Colors.Inputtext,
+  },
+  ButtonColor: {
+    backgroundColor: Colors.buttonColor,
+  },
+  priceTect: {
+    color: Colors.buttonColor,
+  },
+});
+
+export default Styles;
