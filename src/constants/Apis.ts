243242e39329ba6ee7d0e8eb045ae7bf175c73a4
Yuvakishore123 <kolli.yuva@nineleaps.com>
2023-06-22 12:51:56 +0530

    fix(error): Refactoring errors and Major errors fixed

--- src/constants/Apis.ts
+++ src/constants/Apis.ts
@@ -1,4 +1,4 @@
-export const url = 'https://35f7-106-51-70-135.ngrok-free.app/api/v1';
+export const url = 'https://79c2-180-151-121-146.ngrok-free.app/api/v1';
 export const OwnerProductsUrl = `${url}/product/listOwnerProducts`;
 export const UserProductsUrl = `${url}/product/list`;
 export const AddAddressUrl = `${url}/address/add`;
