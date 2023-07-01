f76d95aa7ae55a03046b8bb4a94d4007ea98e4da
Pranay-Reddy007 <pentareddy.pranay@nineleaps.com>
2023-07-01 17:10:52 +0530

    Test: testing done for wishlistReducer

---
+++ __tests__/redux/reducers/wishlistReducer.test.ts
@@ -0,0 +1,63 @@
+import WishlistReducer from '../../../src/redux/reducers/wishlistReducer';
+import {
+  ADD_TO_WISHLIST,
+  REMOVE_FROM_WISHLIST,
+} from '../../../../LeapApp/src/redux/actions/actions';
+
+// Mock AsyncStorage module
+jest.mock('@react-native-async-storage/async-storage', () => ({
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  removeItem: jest.fn(),
+  getAllKeys: jest.fn(),
+}));
+
+describe('WishlistReducer', () => {
+  it('should have initial state of an empty array', () => {
+    const initialState = undefined;
+    const action = {
+      type: 'UNKNOWN_ACTION',
+      payload: null,
+    };
+
+    const newState = WishlistReducer(initialState, action);
+
+    expect(newState).toEqual([]);
+  });
+
+  it('should add an item to the wishlist', () => {
+    const initialState = [];
+    const action = {
+      type: ADD_TO_WISHLIST,
+      payload: 'item1',
+    };
+
+    const newState = WishlistReducer(initialState, action);
+
+    expect(newState).toEqual(['item1']);
+  });
+
+  it('should remove an item from the wishlist', () => {
+    const initialState = ['item1', 'item2', 'item3'];
+    const action = {
+      type: REMOVE_FROM_WISHLIST,
+      payload: 1, // index of item2
+    };
+
+    const newState = WishlistReducer(initialState, action);
+
+    expect(newState).toEqual(['item1', 'item3']);
+  });
+
+  it('should return the same state for unknown action types', () => {
+    const initialState = ['item1', 'item2', 'item3'];
+    const action = {
+      type: 'UNKNOWN_ACTION',
+      payload: null,
+    };
+
+    const newState = WishlistReducer(initialState, action);
+
+    expect(newState).toEqual(initialState);
+  });
+});
