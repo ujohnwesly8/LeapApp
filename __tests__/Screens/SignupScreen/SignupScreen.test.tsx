6dc1b891c2c057315544190a0c721760e2899962
Yuvakishore123 <kolli.yuva@nineleaps.com>
2023-07-03 19:02:55 +0530

    SignupScreen testing Done 75%

---
+++ __tests__/Screens/SignupScreen/SignupScreen.test.tsx
@@ -0,0 +1,100 @@
+import React from 'react';
+
+import {render, fireEvent} from '@testing-library/react-native';
+import SignUpScreen from '../../../src/screens/SignUp/SignupScreen';
+
+jest.mock('@react-native-async-storage/async-storage', () => ({
+  setItem: jest.fn(),
+  getItem: jest.fn(),
+  removeItem: jest.fn(),
+}));
+const mockNavigate = jest.fn();
+jest.mock('@react-navigation/native', () => ({
+  useNavigation: () => ({
+    navigate: mockNavigate,
+  }),
+}));
+
+describe('SignUpScreen', () => {
+  it('renders the signup screen correctly', () => {
+    const {getByPlaceholderText, getByTestId} = render(<SignUpScreen />);
+
+    expect(getByPlaceholderText('Enter First name')).toBeTruthy();
+    expect(getByPlaceholderText('Enter Last name')).toBeTruthy();
+    expect(getByPlaceholderText('Enter email')).toBeTruthy();
+    expect(getByPlaceholderText('Enter Phone number')).toBeTruthy();
+    expect(getByPlaceholderText('Enter password')).toBeTruthy();
+    expect(getByTestId('signup-button')).toBeTruthy();
+
+    expect(getByTestId('login-button')).toBeTruthy();
+  });
+
+  it('validates form inputs and shows error messages', () => {
+    const {getByTestId} = render(<SignUpScreen />);
+
+    const firstNameInput = getByTestId('first-name');
+    const lastNameInput = getByTestId('last-name');
+    const emailInput = getByTestId('Email');
+    const phoneNumberInput = getByTestId('Phone-number');
+    const passwordInput = getByTestId('Password');
+    const signUpButton = getByTestId('signup-button');
+
+    // Attempt to submit the form without entering any data
+
+    expect(getByTestId('first-name')).toBeTruthy();
+    expect(getByTestId('last-name')).toBeTruthy();
+    expect(getByTestId('Email')).toBeTruthy();
+    expect(getByTestId('Phone-number')).toBeTruthy();
+    expect(getByTestId('Password')).toBeTruthy();
+
+    // Enter invalid data in the inputs
+    fireEvent.changeText(firstNameInput, 'John');
+    fireEvent.changeText(lastNameInput, 'Doe');
+    fireEvent.changeText(emailInput, 'invalid-email');
+    fireEvent.changeText(phoneNumberInput, '123456');
+    fireEvent.changeText(passwordInput, 'pass');
+
+    // Attempt to submit the form againx`
+    fireEvent.press(signUpButton);
+  });
+  // Add more test cases for different scenarios as needed
+});
+it('renders the error messages for the input fields', async () => {
+  const {getByPlaceholderText, findByText} = render(<SignUpScreen />);
+
+  const firstNameInput = getByPlaceholderText('Enter First name');
+  const lastNameInput = getByPlaceholderText('Enter Last name');
+  const emailInput = getByPlaceholderText('Enter email');
+  const PhoneNumber = getByPlaceholderText('Enter Phone number');
+  const Password = getByPlaceholderText('Enter password');
+  // Attempt to submit the form without entering the required fields
+  fireEvent.changeText(firstNameInput, '');
+  fireEvent.changeText(lastNameInput, '');
+  fireEvent.changeText(emailInput, '');
+  fireEvent.changeText(PhoneNumber, '');
+  fireEvent.changeText(Password, '');
+  fireEvent(firstNameInput, 'onBlur', {target: {value: ''}});
+  fireEvent(lastNameInput, 'onBlur', {target: {value: ''}});
+  fireEvent(emailInput, 'onBlur', {target: {value: ''}});
+  fireEvent(PhoneNumber, 'onBlur', {target: {value: ''}});
+  fireEvent(Password, 'onBlur', {target: {value: ''}});
+  const firstNameError = await findByText('Enter First Name');
+  const lastNameError = await findByText('Enter LastName');
+  const emailError = await findByText('Enter valid Email');
+  const PhoneNumberError = await findByText('Phone number is required');
+  const PasswordError = await findByText('Please enter password');
+  expect(firstNameError).toBeTruthy();
+  expect(lastNameError).toBeTruthy();
+  expect(emailError).toBeTruthy();
+  expect(PhoneNumberError).toBeTruthy();
+  expect(PasswordError).toBeTruthy();
+});
+test('should navigate to the correct screen when the login button is pressed', () => {
+  const {getByTestId} = render(<SignUpScreen />);
+
+  // Simulate pressing the login button
+  fireEvent.press(getByTestId('login-button'));
+
+  // Assert that the navigate function has been called with the correct screen name
+  expect(mockNavigate).toHaveBeenCalledWith('Login');
+});
