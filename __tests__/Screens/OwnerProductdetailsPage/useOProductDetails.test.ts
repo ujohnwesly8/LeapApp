6a2361b72361244487504f6d23dec6ba7e6d3842
Pranay-Reddy007 <pentareddy.pranay@nineleaps.com>
2023-07-05 15:31:41 +0530

    Test: testing done for OwnerProductsdetailsPage

---
+++ __tests__/Screens/OwnerProductdetailsPage/useOProductDetails.test.ts
@@ -0,0 +1,35 @@
+import {renderHook} from '@testing-library/react-native';
+
+import useOProductDetails from '../../../src/screens/OwnerProductdetailsPage/useOProductDetails';
+import React from 'react';
+
+describe('useOProductDetails', () => {
+  const product = {
+    /* mock product data */
+  };
+  const route = {params: {product}};
+  const navigation = {goBack: jest.fn()};
+  const colorScheme = 'light';
+
+  test('should return the correct values', () => {
+    const mockContext = {colorScheme};
+    jest.spyOn(React, 'useContext').mockReturnValue(mockContext);
+
+    const {result} = renderHook(() => useOProductDetails({route, navigation}));
+
+    expect(result.current.product).toBe(product);
+    expect(result.current.colorScheme).toBe(colorScheme);
+    expect(result.current.goBack).toBeInstanceOf(Function);
+  });
+
+  test('goBack should call navigation.goBack', () => {
+    const mockContext = {colorScheme};
+    jest.spyOn(React, 'useContext').mockReturnValue(mockContext);
+
+    const {result} = renderHook(() => useOProductDetails({route, navigation}));
+
+    result.current.goBack();
+
+    expect(navigation.goBack).toHaveBeenCalled();
+  });
+});
